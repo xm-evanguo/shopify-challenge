f606a287dc17a5bc8121903d805fed341100c9af
Evan Guo <evanguo97@gmail.com>
2021-01-17 03:11:04 -0800

    updated url

--- src/Services/index.js
+++ src/Services/index.js
@@ -1,6 +1,6 @@
 export const getMovieInfo = async (movieTitle) => {
     try {
-        const rawRespond = await fetch(`http://www.omdbapi.com/?apikey=5f098a0e&s=${movieTitle}&type=movie`);
+        const rawRespond = await fetch(`https://www.omdbapi.com/?apikey=5f098a0e&s=${movieTitle}&type=movie`);
         const respond = await rawRespond.json();
         return respond.Search;
     } catch (error) {
