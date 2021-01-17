c054bc039f044c00b8c89beea67fb64f74ac8619
Evan Guo <evanguo97@gmail.com>
2021-01-17 01:46:36 -0800

    formmating

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    printWidth: 160,
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    trailingComma: 'es5',
+    tabWidth: 4,
+    useTabs: false,
+};
